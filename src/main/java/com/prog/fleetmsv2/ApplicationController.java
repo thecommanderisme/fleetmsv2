34b7d5c550b6359f6dd7f58c93ff5886d67f37fd
Daniel <kiddoz_daniel13@yahoo.com>
2024-03-07 00:49:50 +0800

    added new pages

--- src/main/java/com/prog/fleetmsv2/ApplicationController.java
+++ src/main/java/com/prog/fleetmsv2/ApplicationController.java
@@ -7,12 +7,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ApplicationController {
 
 	@GetMapping("/index")
-	public String home() {
-		return "index";
-	}
+	public String home() { return "index"; }
 	
-	@GetMapping("/pages-profile")
-	public String pagesProfile() {
-		return "pages-profile";
-	}
+	@GetMapping("/_layout")
+	public String layout() { return "_layout"; }
+	
+	@GetMapping("/table-basic")
+	public String tableBasic() { return "table-basic"; }
+	
+	@GetMapping("/index2")
+	public String index2() { return "index2"; }
+
+	@GetMapping("/hr")
+	public String hr() { return "/hr/index"; }
+	
+	@GetMapping("/fleet")
+	public String fleet() { return "/fleet/index"; }
+	
+	@GetMapping("/helpdesk")
+	public String helpDesk() { return "/helpdesk/index"; }
+	
+	@GetMapping("/parameters")
+	public String parameters() { return "/parameters/index"; }
+	
+	@GetMapping("/accounts")
+	public String accounts() { return "/accounts/index"; }
+	
+	@GetMapping("/payroll")
+	public String payroll() { return "/payroll/index"; }
 }
